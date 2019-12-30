e3f432602e149e4447969816815825013831ea11
Rustam <rustery@gmail.com>
2019-12-30 16:20:29 +0300

    remove math module

--- raindrops.go
+++ raindrops.go
@@ -1,23 +1,24 @@
-// Exercism side exercise about Raindrops
+// Package raindrops is Exercism side exercise
 package raindrops
 
 import (
-	"math"
 	"strconv"
 )
 
 // Convert converts a number into a string that contains raindrop sounds corresponding to certain potential factors.
-func Convert(i int) (result string) {
+func Convert(i int) string {
 
-	if math.Mod(float64(i), 3) == 0 {
+	result := ""
+
+	if i%3 == 0 {
 		result += "Pling"
 	}
 
-	if math.Mod(float64(i), 5) == 0 {
+	if i%5 == 0 {
 		result += "Plang"
 	}
 
-	if math.Mod(float64(i), 7) == 0 {
+	if i%7 == 0 {
 		result += "Plong"
 	}
 
@@ -25,5 +26,5 @@ func Convert(i int) (result string) {
 		result = strconv.Itoa(i)
 	}
 
-	return
+	return result
 }
